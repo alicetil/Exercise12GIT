9c54f0648ffe111ed8549038ae41795f912fc6d6
alice <alice.tildsley@gmail.com>
2022-02-27 16:29:10 +0000

    added the question for context

--- lotto.py
+++ lotto.py
@@ -1,3 +1,7 @@
+# Write a Python script called lotto.py that will generate and display 6 unique random lottery numbers between 1 and 50.
+# Think about which Python data structure is best suited to store the numbers.
+# Use the Python help() function to find out which function to use from the python standard library called random.
+
 import random
 
 lotto_range = list(range(1,51))
