c1f020017aec9cdb41b8f7f52c046c7a8e68182c
alice <alice.tildsley@gmail.com>
2022-02-28 18:46:06 +0000

    changed to only have unique numbers

--- lotto.py
+++ lotto.py
@@ -5,15 +5,12 @@
 import random
 
 lotto_range = list(range(1,51))
-# print(lotto_range)
 
-draws = 0
-num_list = []
+num_set = set()
 
-while draws < 6:
+while len(num_set) != 6:
     num = random.choice(lotto_range)
     print(num)
-    num_list += [num]
-    draws = draws + 1
+    num_set.add(num)
 
-print(num_list)
+print(num_set)
